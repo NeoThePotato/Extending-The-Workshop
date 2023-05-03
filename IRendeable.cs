53f786aab747e28ec6ada4ce399a8d5c35623847
NeoThePotato <neorayb@gmail.com>
2023-05-03 10:45:27 +0300

    Added base for rendering stuff.

---
+++ IRendeable.cs
@@ -0,0 +1,11 @@
+﻿namespace Visuals.Render
+{
+
+	/// <summary>
+	/// Interface which can be rendered into a Renderer
+	/// </summary>
+	interface IRenderable
+	{
+		public char[,] Render();
+	}
+}
---
+++ Renderer.cs
@@ -0,0 +1,30 @@
+﻿namespace Visuals.Render
+{
+
+	/// <summary>
+	/// Handles rendering of elements into the console
+	/// </summary>
+	class Renderer
+	{
+		private char[,] BufferCache { get; set; }
+		private Element[] Elements { get; set; }
+
+		public Renderer(int elementsCount, int dimX, int dimY)
+		{
+			BufferCache = new char[dimX, dimY];
+			Elements = new Element[elementsCount];
+		}
+
+		public void Render() { }
+	}
+
+	/// <summary>
+	/// Contains a renderable element with info of where it should be rendered on the screen-space
+	/// </summary>
+	struct Element
+	{
+		public int x, y;
+		public IRenderable renderable;
+	}
+
+}
