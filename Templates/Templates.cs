233deac8eb4298f12155cd85411e444e04246999
NeoThePotato <neorayb@gmail.com>
2023-05-12 00:05:58 +0300

    Added equipment templates.

---
+++ Templates/Templates.cs
@@ -0,0 +1,19 @@
+﻿using Combat.Equipment;
+
+namespace Templates
+{
+
+	static class Weapons
+	{
+		public static Weapon nothing = new Weapon("", 0);
+		public static Weapon rustedBlade = new Weapon("Rusted Blade", 1);
+	}
+
+	static class Armors
+	{
+		public static Armor nothing = new Armor("", 0);
+		public static Armor rustedChestplate = new Armor("Rusted Chestplate", 1);
+		public static Armor rustedBuckler = new Armor("Rusted Buckler", 1);
+	}
+
+}
