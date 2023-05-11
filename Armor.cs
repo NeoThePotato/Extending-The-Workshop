9bc63262ffcc07a8d78d50853d57181b21caf1a2
NeoThePotato <neorayb@gmail.com>
2023-05-11 23:03:41 +0300

    Renamed "Shield" to "Armor".

--- Armor.cs
+++ Armor.cs
@@ -1,20 +1,15 @@
 ﻿namespace Combat
 {
 
-	class Shield
+	class Armor
 	{
 		public string Name { get; private set; }
 		public int Defense { get; private set; }
 
-		public Shield(string name, int shield)
+		public Armor(string name, int shield)
 		{
 			Name = name;
 			Defense = shield;
-		}
-
-		public int GetUnblockedDamage(int damage)
-		{
-			return Math.Max(0, damage - Defense);
 		}
 
 		public string GetStats()
