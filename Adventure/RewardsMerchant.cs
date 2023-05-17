4cc9059a1cce9c774a10b32ab346afa5908a92ed
NeoThePorato <neorayb@gmail.com>
2023-05-17 22:59:35 +0300

    Implementing RewardsMerchant.

--- Adventure/RewardsMerchant.cs
+++ Adventure/RewardsMerchant.cs
@@ -2,6 +2,21 @@
 {
 	class RewardsMerchant
 	{
-		// TODO Implement
+		public int Level { get; private set; } = 0;
+
+		public RewardsMerchant()
+		{
+
+		}
+
+        public void RaiseLevel()
+        {
+			RaiseLevel(1);
+        }
+
+        public void RaiseLevel(int amount)
+		{
+			Level += amount;
+		}
 	}
 }
